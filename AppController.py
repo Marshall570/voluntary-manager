5bed643a1ebb13607d98d337b4afb0ff0aadd459
Gustavo Fernandes <gustavosergio2001@gmail.com>
2020-07-01 16:22:10 -0300

    Search and Delete - DONE

--- AppController.py
+++ AppController.py
@@ -144,6 +144,62 @@ class AppController:
         finally:
             db.close_connection()
 
+    def update(self, model, index):
+        try:
+            conn = db.create_connection()
+            cursor = conn.cursor()
+            voluntary_id = cursor.execute('SELECT COUNT(*) FROM voluntaries').fetchone()[0] + 1
+
+            update_string = f'''UPDATE voluntaries SET             
+            nome = '{model.name}',
+            nome_do_pai = '{model.father}',
+            nome_da_mae = '{model.mother}',
+            endereco = '{model.address}',
+            numero = '{model.number}',
+            complemento = '{model.complement}',
+            bairro = '{model.neighbourhood}',
+            cidade = '{model.city}',
+            estado = '{model.state}',
+            cep = '{model.postal_code}',
+            telefone_residencial = '{model.home_phone}',
+            telefone_celular = '{model.mobile_phone}',
+            cpf = '{model.cpf}',
+            rg = '{model.rg}',
+            emissor = '{model.emitter}',
+            cidade_natal = '{model.home_town}',
+            estado_natal = '{model.home_state}',
+            data_de_nascimento = '{model.birth_date}',
+            estado_civil = '{model.civil_state}',
+            genero = '{model.gender}',
+            escolaridade = '{model.scholarship}',
+            email = '{model.email}',
+            cursando = '{model.course}',
+            empresa = '{model.company_name}',
+            ocupacao = '{model.ocupation}',
+            tempo_de_empresa = '{model.company_time}',
+            endereco_empresa = '{model.company_address}',
+            bairro_empresa = '{model.company_neighbourhood}',
+            numero_empresa = '{model.company_number}',
+            cidade_empresa = '{model.company_city}',
+            estado_empresa = '{model.company_state}',
+            cep_empresa = '{model.company_postal_code}',
+            telefone_empresa = '{model.company_phone}'
+            WHERE id = {index}'''
+
+            cursor.execute(update_string)
+            conn.commit()
+
+            root = tkinter.Tk()
+            root.withdraw()
+            messagebox.showinfo('SUCESSO', 'Registro de voluntário editado com sucesso!')
+            tkinter.Tk().destroy()
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            db.close_connection()
+
     def get_total(self):
         try:
             conn = db.create_connection()
@@ -151,6 +207,60 @@ class AppController:
             total = cursor.execute('SELECT COUNT(*) FROM voluntaries').fetchone()[0]
 
             return total
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            db.close_connection()
+
+    def delete(self, index):
+        try:
+           conn = db.create_connection()
+           cursor = conn.cursor()
+           cursor.execute(f'DELETE FROM voluntaries WHERE id = {index}')
+
+           cursor.execute(f'UPDATE voluntaries SET id = id - 1 WHERE id > {index}')
+
+           conn.commit()
+
+           root = tkinter.Tk()
+           root.withdraw()
+           messagebox.showinfo('SUCESSO', 'Registro deletado com sucesso!')
+           tkinter.Tk().destroy()
+        
+        except Exception as e:
+            print(e)
+
+        finally:
+            db.close_connection()
+
+    def id_search(self, word):
+        try:
+            conn = db.create_connection()
+            cursor = conn.cursor()
+            result = cursor.execute(f"SELECT id, nome FROM voluntaries WHERE nome LIKE '%{word}%'").fetchall()
+
+            # print(result)
+
+            if result == []:
+                root = tkinter.Tk()
+                root.withdraw()
+                messagebox.showwarning('ATENÇÃO', 'Não foram encontrados resultados com o termo utilizado.')
+                tkinter.Tk().destroy()
+
+            else:
+                message = ''
+
+                for row in result:
+                    for column in row:
+                        message += str(column)
+                        message += ' - '                
+
+                root = tkinter.Tk()
+                root.withdraw()
+                messagebox.showinfo('ATENÇÃO', message[:-3])
+                tkinter.Tk().destroy()                
 
         except Exception as e:
             print(e)
